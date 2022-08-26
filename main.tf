4f77a0ed9d74b042642aff8b292033e0bd63b527
vipinable <vipinable@gmail.com>
2022-08-26 14:35:02 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,19 @@
-#testing
+resource "aws_imagebuilder_component" "this" {
+  data = yamlencode({
+    phases = [{
+      name = "build"
+      steps = [{
+        action = "ExecuteBash"
+        inputs = {
+          commands = ["echo 'hello world'"]
+        }
+        name      = "example"
+        onFailure = "Continue"
+      }]
+    }]
+    schemaVersion = 1.0
+  })
+  name     = "example"
+  platform = "Linux"
+  version  = "1.0.0"
+}
