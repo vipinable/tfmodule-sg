6f31fd552f8f63773892acb573f0b73be56ce159
vipinable <vipinable@gmail.com>
2022-08-29 15:21:14 +0200

    added policy for image builder

--- main.tf
+++ main.tf
@@ -1,22 +1,25 @@
-resource "aws_imagebuilder_component" "this" {
-  data = yamlencode({
-    phases = [{
-      name = "build"
-      steps = [{
-        action = "ExecuteBash"
-        inputs = {
-          commands = ["echo 'hello world'"]
-        }
-        name      = "example"
-        onFailure = "Continue"
-      }]
-    }]
-    schemaVersion = 1.0
-  })
-  name     = "example"
-  platform = "Linux"
-  version  = "1.0.0"
-  
+resource "aws_iam_service_linked_role" "imagebuilderRole" {
+  aws_service_name = "imagebuilder.amazonaws.com"
+}
+
+resource "aws_iam_role" "imagebuilderProfileRole" {
+  name                 = format("iam-role-%s-imagebuilderProfileRole", "app")
+  assume_role_policy   = data.aws_iam_policy_document.imagebuilderEc2profilePolicy.json
+}
+
+resource "aws_iam_role_policy_attachment" "EC2InstanceProfileForImageBuilder" {
+  role       = aws_iam_role.imagebuilderProfileRole.name
+  policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilder"
+}
+
+data "aws_iam_policy_document" "imagebuilderEc2profilePolicy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
 }
 
 resource "null_resource" "resource1" {
